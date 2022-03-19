18dfedabcbb7ff2e8a2a12b7a416e8a2367ce93b
Marcin Wierzchowski <mwierzch@gmail.com>
2022-03-19 16:10:19 +0100

    Add initial spring boot application

---
+++ src/main/java/com/github/mwierzchowski/weather/store/WeatherStoreApplication.java
@@ -0,0 +1,11 @@
+package com.github.mwierzchowski.weather.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherStoreApplication.class, args);
+    }
+}
