922054642d409a53bfaaa93f0a59dc0639640d2d
Marcin Wierzchowski <mwierzch@gmail.com>
2022-03-20 23:09:35 +0100

    Refactor configuration

--- src/main/java/com/github/mwierzchowski/weather/store/Application.java
+++ src/main/java/com/github/mwierzchowski/weather/store/Application.java
@@ -2,16 +2,9 @@ package com.github.mwierzchowski.weather.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/v3/api-docs/**");
-    }
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/com/github/mwierzchowski/weather/store/config/ApiConfig.java
@@ -0,0 +1,25 @@
+package com.github.mwierzchowski.weather.store.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Bean
+    OpenAPI openAPI(Environment env) {
+        var appInfo = new Info()
+                .title(env.getProperty("info.app.name"))
+                .description(env.getProperty("info.app.description"));
+        return new OpenAPI().info(appInfo);
+    }
+}
