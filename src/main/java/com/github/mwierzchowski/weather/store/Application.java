44db065f226c3837098ea1682c3c9ff3d21eb324
Marcin Wierzchowski <mwierzch@gmail.com>
2022-03-24 13:19:05 +0100

    Fixes

--- src/main/java/com/github/mwierzchowski/weather/store/Application.java
+++ src/main/java/com/github/mwierzchowski/weather/store/Application.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+
+
+
+
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
