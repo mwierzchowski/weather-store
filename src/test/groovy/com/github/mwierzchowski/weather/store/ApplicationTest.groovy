52338bdc2a9f23bb19fb31a714d99f8769ffd948
Marcin Wierzchowski <mwierzch@gmail.com>
2022-04-09 19:45:49 +0200

    Simplify Application test

--- src/test/groovy/com/github/mwierzchowski/weather/store/ApplicationTest.groovy
+++ src/test/groovy/com/github/mwierzchowski/weather/store/ApplicationTest.groovy
@@ -3,11 +3,9 @@ package com.github.mwierzchowski.weather.store
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
-import spock.lang.Subject
 
 @SpringBootTest
 class ApplicationTest extends Specification {
-    @Subject
     @Autowired
     Application application
 
