c6f448b8d0f12096674791c8c7b07ad9a3e070e9
Marcin Wierzchowski <mwierzch@gmail.com>
2022-04-23 18:41:42 +0200

    Simplify app configuration

--- src/main/java/com/github/mwierzchowski/weather/store/Application.java
+++ src/main/java/com/github/mwierzchowski/weather/store/Application.java
@@ -1,11 +1,7 @@
 package com.github.mwierzchowski.weather.store;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,14 +14,6 @@ public class Application implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**");
-    }
-
-    @Bean
-    OpenAPI openApi(final Environment env) {
-        var appInfo = new Info()
-                .title(env.getProperty("info.app.name"))
-                .description(env.getProperty("info.app.description"));
-        return new OpenAPI().info(appInfo);
     }
 
     public static void main(String[] args) {
