be44288d1c099e11e2db464372d8e1b5714261fa
Marcin Wierzchowski <mwierzch@gmail.com>
2022-04-26 11:00:02 +0200

    Switch to embeded Swagger UI to avoid issues with CORS

--- src/main/java/com/github/mwierzchowski/weather/store/Application.java
+++ src/main/java/com/github/mwierzchowski/weather/store/Application.java
@@ -2,20 +2,11 @@ package com.github.mwierzchowski.weather.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableTransactionManagement
-public class Application implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
